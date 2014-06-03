ba5a9d3dd4aef5a4491df010b06e88e1b79f1971
Adam May <adam@livefront.com>
2014-06-02 21:07:52 -0500

    Updated Lab3.2 and complete with new networking code.

--- Week3/Source/SmarticleComplete/Smarticle/ArticlesController.m
+++ Week3/Source/SmarticleComplete/Smarticle/ArticlesController.m
@@ -7,14 +7,6 @@
 ///
 static NSString * const ArticlesControllerFavoritesFilename = @"Favorites.plist";
 
-/// The API key to use for all requests made by this controller.
-///
-static NSString * const ArticlesControllerNYTimesAPIKey = @"00edfc4660e9ccd3d484598a6aafd603:11:68426625";
-
-/// The base URI for all requests made by this controller.
-///
-static NSString * const ArticlesControllerNYTimesBaseURI = @"http://api.nytimes.com/svc/mostpopular/v2/";
-
 /// The shared singleton instance, initialized once on the first call to sharedInstance.
 ///
 static ArticlesController *ArticlesControllerSharedInstance;
@@ -55,20 +47,6 @@ static ArticlesController *ArticlesControllerSharedInstance;
 ///     favorites array. Otherwise returns NO.
 ///
 - (BOOL)isFavoriteArticle:(Article *)article;
-
-/// Processes a successful response from a request to get articles.
-///
-/// Attempts to parse the response of a successful response containing a list of articles retrieved
-/// from the API.
-///
-/// @param data
-///     The data from the successful response.
-///
-/// @param completionHandler
-///     The completion handler to call when handling is complete.
-///
-- (void)processArticlesWithData:(NSData *)data
-              completionHandler:(ArticlesControllerCompletionHandler)completionHandler;
 
 /// Reads the favorites array from disk via NSKeyedUnarchiver.
 ///
@@ -126,43 +104,6 @@ static ArticlesController *ArticlesControllerSharedInstance;
     return self.favorites;
 }
 
-- (void)getArticlesForResourceType:(NSString *)resourceType
-                            offset:(NSUInteger)offset
-                 completionHandler:(ArticlesControllerCompletionHandler)completionHandler
-{
-    static NSUInteger DefaultTimePeriod = 7;
-    
-    NSString *requestString = [ArticlesControllerNYTimesBaseURI stringByAppendingFormat:
-                               @"%@/all-sections/%d.json?api-key=%@&offset=%d",
-                               resourceType,
-                               DefaultTimePeriod,
-                               ArticlesControllerNYTimesAPIKey,
-                               offset];
-    
-    NSURL *url = [NSURL URLWithString:requestString];
-    
-    NSURLSessionDataTask *task = [[NSURLSession sharedSession]
-                                  dataTaskWithURL:url
-                                  completionHandler:^(NSData *data,
-                                                      NSURLResponse *response,
-                                                      NSError *error)
-    {
-        if (error)
-        {
-            dispatch_async(dispatch_get_main_queue(), ^{
-                completionHandler(nil, @0, error);
-            });
-        }
-        else
-        {
-            [self processArticlesWithData:data
-                        completionHandler:completionHandler];
-        }
-    }];
-    
-    [task resume];
-}
-
 - (void)removeFavoriteArticle:(Article *)article
 {
     article.isFavorite = NO;
@@ -208,34 +149,6 @@ static ArticlesController *ArticlesControllerSharedInstance;
 {
     Article *favorite = [self favoriteArticleWithURLString:article.url];
     return favorite != nil;
-}
-
-- (void)processArticlesWithData:(NSData *)data
-              completionHandler:(ArticlesControllerCompletionHandler)completionHandler
-
-{
-    __block NSError *serializationError;
-    id json = [NSJSONSerialization JSONObjectWithData:data
-                                              options:0
-                                                error:&serializationError];
-    dispatch_async(dispatch_get_main_queue(), ^{
-        if (serializationError)
-        {
-            completionHandler(nil, @0, serializationError);
-        }
-        else
-        {
-            NSNumber *numResults = json[@"num_results"];
-            NSArray *articles = [Article articlesForJSON:json];
-
-            for (Article *article in articles)
-            {
-                article.isFavorite = [self isFavoriteArticle:article];
-            }
-
-            completionHandler(articles, numResults, nil);
-        }
-    });
 }
 
 - (NSArray *)readFavoriteArticles
--- Week3/Source/SmarticleLab3.2/Smarticle/ListViewController.m
+++ Week3/Source/SmarticleLab3.2/Smarticle/ListViewController.m
@@ -3,109 +3,57 @@
 #import "ArticleCell.h"
 #import "ArticlesController.h"
 #import "DetailViewController.h"
+#import "ArticleProvider.h"
 
-@interface ListViewController ()
+@interface ListViewController () <ArticleProviderDelegate>
 
 #pragma mark - Private Properties
 
-/// List of articles to display in the list.
-///
-@property (nonatomic, strong) NSArray *articles;
-
-/// The total number of articles available in the results set.
-///
-@property (nonatomic, assign) NSUInteger totalNumberOfArticles;
-
-/// The currently loaded offset from the current results set.
-///
-@property (nonatomic, assign) NSUInteger currentOffset;
-
-/// The offset that is currently loading or will be loaded the next time reloadArticles is called.
-///
-@property (nonatomic, assign) NSUInteger loadingOffset;
-
-#pragma mark - Private Methods
-
-/// Reloads articles from the articles controller.
-///
-/// If there are more articles to load and no articles are currently loading, reloads the articles
-/// from the articles controller asynchronously.
-///
-- (void)reloadArticles;
+@property (nonatomic, strong) ArticleProvider *articles;
 
 @end
 
 @implementation ListViewController
 
-#pragma mark - Private Methods
+#pragma mark - ArticlesProviderDelegate
 
-- (void)reloadArticles
+- (void)provider:(id)provider didLoadDataAtIndexes:(NSIndexSet *)indexes
 {
-    if (self.currentOffset != self.loadingOffset)
-    {
-        [[ArticlesController sharedInstance] getArticlesForResourceType:@"mostemailed"
-                                                                 offset:self.loadingOffset
-                                                      completionHandler:^(NSArray *articles,
-                                                                          NSNumber *numberOfResults,
-                                                                          NSError *error)
-         {
-             self.currentOffset = self.loadingOffset;
-             
-             if (error)
-             {
-                 UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Drat!"
-                                                                 message:@"We're sorry, we can't get any articles at the moment. Please try again later."
-                                                                delegate:nil
-                                                       cancelButtonTitle:@"OK"
-                                                       otherButtonTitles:nil];
-                 [alert show];
-             }
-             else
-             {
-                 if (self.articles == nil)
-                 {
-                     self.articles = articles;
-                     self.totalNumberOfArticles = [numberOfResults unsignedIntegerValue];
-                 }
-                 else
-                 {
-                     self.articles = [self.articles arrayByAddingObjectsFromArray:articles];
-                 }
-                 
-                 [self.tableView reloadData];
-             }
-         }];
-    }
+    [self.tableView reloadData];
 }
 
-#pragma mark - UIScrollViewDelegate
-
-- (void)scrollViewDidScroll:(UIScrollView *)scrollView
+- (void)provider:(id)provider didFailWithError:(NSError *)error
 {
-    if (scrollView.contentOffset.y >= scrollView.contentSize.height - scrollView.frame.size.height)
-    {
-        if (self.loadingOffset == self.currentOffset)
-        {
-            self.loadingOffset = self.currentOffset + 20;
-
-            [self reloadArticles];
-        }
-    }
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Drat!"
+                                                    message:@"We're sorry, we can't get any articles at the moment. Please try again later."
+                                                   delegate:nil
+                                          cancelButtonTitle:@"OK"
+                                          otherButtonTitles:nil];
+    [alert show];
 }
 
 #pragma mark - UITableViewDataSource
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return self.articles.count;
+    return self.articles.numberOfArticles;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     static NSString *CellIdentifier = @"ArticleCell";
     ArticleCell *cell = (ArticleCell *)[tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
-    Article *article = [self.articles objectAtIndex:indexPath.row];
-    cell.titleLabel.text = article.title;
+    Article *article = [self.articles articleAtIndex:indexPath.row];
+    
+    if (article)
+    {
+        cell.titleLabel.text = article.title;
+    }
+    else
+    {
+        cell.titleLabel.text = nil;
+    }
+    
     return cell;
 }
 
@@ -113,10 +61,14 @@
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    Article *article = [self.articles objectAtIndex:indexPath.row];
-    DetailViewController *detailViewController = [[DetailViewController alloc] initWithNibName:@"DetailViewController" bundle:nil];
-    detailViewController.article = article;
-    [self.navigationController pushViewController:detailViewController animated:YES];
+    Article *article = [self.articles articleAtIndex:indexPath.row];
+    
+    if (article)
+    {
+        DetailViewController *detailViewController = [[DetailViewController alloc] initWithNibName:@"DetailViewController" bundle:nil];
+        detailViewController.article = article;
+        [self.navigationController pushViewController:detailViewController animated:YES];
+    }
 }
 
 #pragma mark - UIViewController
@@ -124,13 +76,10 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-}
-
-- (void)viewWillAppear:(BOOL)animated
-{
-    [super viewWillAppear:animated];
     
-    [self reloadArticles];
+    self.articles = [[ArticleProvider alloc] initWithResourceType:ArticleProviderResourceTypeMostEmailed];
+    self.articles.delegate = self;
+    [self.articles reload];
 }
 
 @end
