601a410fc2d1aa51349e4f34e13c4e75f69ee374
Sam Kirchmeier <sam@livefront.com>
2014-06-09 17:37:21 -0500

    Removed obsolete source for week 4

--- Week4/Source/SmarticleComplete/Smarticle/MainViewController.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-
-/// Main view controller to display the initial view in the app.
-///
-@interface MainViewController : UIViewController
-
-@end
--- Week4/Source/SmarticleLab4.1/Smarticle/ListViewController.h
+++
@@ -1,9 +0,0 @@
-#import <UIKit/UIKit.h>
-
-/// Custom table view controller to display a list of articles.
-///
-@interface ListViewController : UITableViewController
-
-@property (nonatomic, assign) ArticleType articleType;
-
-@end
