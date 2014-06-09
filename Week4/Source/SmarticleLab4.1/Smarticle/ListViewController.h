280e4c8e33b336c97088a0fcac597a4ba87a3155
Sam Kirchmeier <sam@livefront.com>
2014-06-09 16:25:36 -0500

    Added source for Lab 4.1

---
+++ Week4/Source/SmarticleLab4.1/Smarticle/ListViewController.h
@@ -0,0 +1,9 @@
+#import <UIKit/UIKit.h>
+
+/// Custom table view controller to display a list of articles.
+///
+@interface ListViewController : UITableViewController
+
+@property (nonatomic, assign) ArticleType articleType;
+
+@end
---
+++ Week4/Source/SmarticleLab4.1/Smarticle/MainViewController.h
@@ -0,0 +1,7 @@
+#import <UIKit/UIKit.h>
+
+/// Main view controller to display the initial view in the app.
+///
+@interface MainViewController : UIViewController
+
+@end
